7849aeb218a7f37fb5f6ea4f425cd6754d556318
James Halliday <mail@substack.net>
2014-09-08 20:40:52 +0100

    use-case coverage

---
+++ example/build-server/999.js
@@ -0,0 +1,23 @@
+/*
+
+  use cases covered:
+  
+  * batch queue (batchdb)
+  * batch queue for shell scripts
+  * batch queue for shell scripts with http api
+  * batch queue for shell scripts with http ui
+  * batch queue for shell scripts with http api and ui
+  * batch queue scripts with http api
+  * batch queue scripts with http ui
+  * batch queue scripts with http api and http ui
+  * shell command capture and parsing
+
+  packages written:
+  
+  * batchdb
+  * batchdb-shell
+  * shell-pack
+  * batchdb-web-api
+  * batchdb-web-ui
+
+*/
